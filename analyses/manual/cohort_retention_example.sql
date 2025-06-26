fb307cec2a05071cd4584179a9df0fa50c0a4b6a
Sean O'Toole <seanotoole74@gmail.com>
2025-06-26 06:24:16 +0000

    add retention over time & amend cohort retention aliases

--- analyses/manual/cohort_retention_example.sql
+++ analyses/manual/cohort_retention_example.sql
@@ -1,9 +1,9 @@
 --retention from initial cohort example
 
+--cohort of customers 
+--in this example customers acquired in december 2023 from brazil
 with cohort as 
 (
-  --cohort of customers 
-  --in this example customers acquired in december 2023 from brazil
   select 
     customer_id
   from 
@@ -26,13 +26,13 @@ with cohort as
 select 
   format_date('%m-%Y',rr.from_date) as retention_month,
   count(distinct c.customer_id) as count_cohort_customer,
-  count(distinct ma.customer_id) as count_retention_customer,
-  round(count(distinct ma.customer_id) / count(distinct c.customer_id),3) as retention_rate,
+  count(distinct a.customer_id) as count_retention_customer,
+  round(count(distinct a.customer_id) / count(distinct c.customer_id),3) as retention_rate,
 from cohort c
   cross join retention_range rr
-  left join {{ref("fact_manual__activity")}} ma
-    on c.customer_id = ma.customer_id
-      and range_overlaps(ma.subscription_range , rr.month_range)
+  left join {{ref("fact_manual__activity")}} a
+    on c.customer_id = a.customer_id
+      and range_overlaps(a.subscription_range , rr.month_range)
 group by 
   1
 order by 
